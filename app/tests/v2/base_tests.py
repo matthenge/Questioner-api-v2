3976639141a8c60f5e4cf416a8b03eaf7a13e821
matthenge <matthenge@gmail.com>
2019-01-20 15:24:20 +0300

    [chore #163256715]Modify user base tests

--- app/tests/v2/base_tests.py
+++ app/tests/v2/base_tests.py
@@ -15,27 +15,7 @@ class BaseTest(unittest.TestCase):
         self.app_context = self.app
         self.app.testing = True
 
-        self.meetup = {
-            "location": "Kisumu",
-            "images": "www.image1.com",
-            "topic": "data science",
-            "happeningOn": "2019-10-22 10:00",
-            "userId": 1
-        }
-        self.oneMeetup = {
-            "location": "Kisumu",
-            "images": "www.image1.com",
-            "topic": "data science",
-            "happeningOn": "2018-10-22 10:00",
-            "userId": 1
-        }
-        self.duplicateuser = {
-            "email": "general@gmail.com",
-            "username": "genmatheng",
-            "password": "Qwerty123!",
-            "confirm_password": "Qwerty123!"
-        }
-        self.admin = {
+        self.oneUser = {
             "firstname": "Nelson",
             "lastname": "Mandela",
             "othername": "lohilala",
@@ -45,129 +25,75 @@ class BaseTest(unittest.TestCase):
             "password": "Qwerty123!",
             "confirm_password": "Qwerty123!"
         }
-        self.email = {
-            "email": "martingmail.com",
-            "username": "martial",
+        self.admin = {
+            "firstname": "Nelson",
+            "lastname": "Mandela",
+            "othername": "lohilala",
+            "email": "marn@gmail.com",
+            "phoneNumber": "0711333666",
+            "username": "pogba",
             "password": "Qwerty123!",
             "confirm_password": "Qwerty123!"
         }
-        self.question = {
-            "createdBy": 1,
-            "meetupId": 1,
-            "title": "What is data science",
-            "body": "please explain data science in length"
+        self.email = {
+            "firstname": "Nelson",
+            "lastname": "Mandela",
+            "othername": "lohilala",
+            "email": "martingmail.com",
+            "phoneNumber": "0711333666",
+            "username": "nelly",
+            "password": "Qwerty123!",
+            "confirm_password": "Qwerty123!"
         }
         self.emptyStrings = {
-            "createdBy": 1,
-            "meetupId": 1,
-            "title": " ",
-            "body": "please explain data science in length"
-        }
-        self.rsvp = {
-            "userId": 1
+            "firstname": "Nelson",
+            "lastname": "",
+            "othername": "",
+            "email": "tata@gmail.com",
+            "phoneNumber": "0711333666",
+            "username": "tata",
+            "password": "Qwerty123!",
+            "confirm_password": "Qwerty123!"
         }
         self.login = {
-            "username": "genmatheng",
+            "username": "martial",
             "password": "Qwerty123!"
         }
         self.noUser = {
-            "username": "dedan",
-            "password": "kimathi"
+            "username": "putin",
+            "password": "Qwerty123!"
         }
         self.weakpass = {
-            "email": "martin@gmail.com",
-            "username": "martial",
-            "password": "qwerty123",
-            "confirm_password": "qwerty123"
-        }
-        self.upcoming = {
-            "location": "Kisumu",
-            "images": "www.image1.com",
-            "topic": "data science",
-            "happeningOn": "2020-10-22 10:00",
-            "userId": 1
+            "firstname": "Nelson",
+            "lastname": "Mandela",
+            "othername": "lohilala",
+            "email": "lucas@gmail.com",
+            "phoneNumber": "0711333777",
+            "username": "torreira",
+            "password": "qwerty",
+            "confirm_password": "qwerty"
         }
 
     def signup(self):
         """user registration"""
         res = self.client.post(
             '/api/v2/auth/signup',
-            data=json.dumps(self.duplicateuser),
-            content_type='application/json')
-        return res
-
-    def create_meetup(self):
-        """meetup creation"""
-        res = self.client.post(
-            '/api/v2/meetups',
-            data=json.dumps(self.meetup),
-            content_type='application/json')
-        return res
-
-    def post_question(self):
-        """post question"""
-        res = self.client.post(
-            '/api/v2/questions',
-            data=json.dumps(self.question),
+            data=json.dumps(self.oneUser),
             content_type='application/json')
         return res
 
     def post_empty_string(self):
         """post question with empty fields"""
         res = self.client.post(
-            '/api/v2/questions',
+            '/api/v2/auth/signup',
             data=json.dumps(self.emptyStrings),
             content_type='application/json')
-        return res
-
-    def reserve_space(self):
-        """reserve attendance"""
-        res = self.client.post(
-            '/api/v2/meetups/1/rsvps',
-            data=json.dumps(self.rsvp),
-            content_type='application/json')
-        return res
-
-    def get_meetup(self):
-        """Fetch specific meetup"""
-        res = self.client.get(
-            '/api/v2/meetups/1')
-        return res
-
-    def get_nonexistent_meetup(self):
-        """Fetch meetup that does not exist"""
-        res = self.client.get(
-            '/api/v2/meetups/10')
-        return res
-
-    def get_all_meetups(self):
-        """Fetch all meetups"""
-        res = self.client.get(
-            '/api/v2/meetups')
-        return res
-
-    def get_all_upcoming_meetups(self):
-        """Fetch all upcoming meetups"""
-        res = self.client.get(
-            '/api/v2/meetups/upcoming')
-        return res
-
-    def upvote_question(self):
-        """Upvote a question"""
-        res = self.client.patch(
-            '/api/v2/questions/1/upvote')
-        return res
-
-    def downvote_question(self):
-        """Downvote a question"""
-        res = self.client.patch(
-            '/api/v2/questions/1/downvote')
         return res
 
     def user_login(self):
         """login user"""
         res = self.client.post(
-            '/api/v2/auth/users/login',
+            '/api/v2/auth/login',
             data=json.dumps(self.login),
             content_type='application/json')
         return res
@@ -175,7 +101,7 @@ class BaseTest(unittest.TestCase):
     def register(self):
         """user registration"""
         res = self.client.post(
-            '/api/v2/auth/users',
+            '/api/v2/auth/signup',
             data=json.dumps(self.admin),
             content_type='application/json')
         return res
@@ -183,32 +109,16 @@ class BaseTest(unittest.TestCase):
     def weak_password(self):
         """user registration"""
         res = self.client.post(
-            '/api/v2/auth/users',
+            '/api/v2/auth/signup',
             data=json.dumps(self.weakpass),
-            content_type='application/json')
-        return res
-
-    def past_meetupdate(self):
-        """Past happeningOn date"""
-        res = self.client.post(
-            '/api/v2/meetups',
-            data=json.dumps(self.oneMeetup),
             content_type='application/json')
         return res
 
     def invalid_email(self):
         """inavlid email"""
         res = self.client.post(
-            '/api/v2/auth/users',
+            '/api/v2/auth/signup',
             data=json.dumps(self.email),
-            content_type='application/json')
-        return res
-
-    def future_meetups(self):
-        """Past happeningOn date"""
-        res = self.client.post(
-            '/api/v2/meetups',
-            data=json.dumps(self.upcoming),
             content_type='application/json')
         return res
 
@@ -217,14 +127,6 @@ class BaseTest(unittest.TestCase):
         res = self.client.post(
             '/api/v2/auth/login',
             data=json.dumps(self.noUser),
-            content_type='application/json')
-        return res
-
-    def post_comment(self):
-        """Post a comment"""
-        res = self.client.post(
-            '/api/v2/comments',
-            data=json.dumps(self.question),
             content_type='application/json')
         return res
 
