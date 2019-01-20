85766d5e9c6d9d332db03e4168bcfd5de34e66ea
matthenge <matthenge@gmail.com>
2019-01-20 15:25:01 +0300

    [chore #163256715]Modify main user tests

--- app/tests/v2/test_views.py
+++ app/tests/v2/test_views.py
@@ -16,114 +16,34 @@ class TestViews(BaseTest):
         response = self.user_login()
         self.assertEqual(response.status_code, 200)
 
-    def test_create_meetup(self):
-        """Test create meetup endpoint"""
-        self.register()
-        response = self.create_meetup()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "Meetup Created Successfully")
-        self.assertEqual(response.status_code, 201)
-
-    def test_get_meetup(self):
-        """Test get specific meetup endpoint"""
-        response = self.get_meetup()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["Message"], "Success")
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_nonexistent_meetup(self):
-        """Test get nonexistent meetup"""
-        response = self.get_nonexistent_meetup()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"], "Meetup does not exist")
-        self.assertEqual(response.status_code, 404)
-
-    def test_get_all_meetups(self):
-        """Test get all meetups endpoint"""
-        self.register()
-        self.create_meetup()
-        response = self.get_all_meetups()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["Message"], "Success")
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_question(self):
-        """Test post question endpoint"""
-        self.create_meetup()
-        response = self.post_question()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "Question Posted Successfully")
-        self.assertEqual(response.status_code, 201)
-
-    def test_upvote_question(self):
-        """Test Upvote question endpoint"""
-        self.post_question()
-        response = self.upvote_question()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "Upvote Successful")
-        self.assertEqual(response.status_code, 200)
-
-    def test_downvote_question(self):
-        """Test Downvote question endpoint"""
-        self.post_question()
-        self.upvote_question()
-        response = self.downvote_question()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "Downvote Successful")
-        self.assertEqual(response.status_code, 200)
-
-    def test_reserve_space(self):
-        """Test RSVP endpoint"""
-        self.create_meetup()
-        response = self.reserve_space()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "RSVP created")
-        self.assertEqual(response.status_code, 201)
-
     def test_repeat_username(self):
         """Test signup repeat username"""
+        self.signup()
         response = self.signup()
         result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"], "Username already exists")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(result["error"], "Username already exists")
+        self.assertEqual(response.status_code, 400)
 
     def test_weak_password(self):
         """Test weak password"""
         response = self.weak_password()
         result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"], message)
-        self.assertEqual(response.status_code, 403)
-
-    def test_past_meetup_date(self):
-        """Test past meetup date"""
-        response = self.past_meetupdate()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"], "New Meetup cannot be in the past")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(result["error"], message)
+        self.assertEqual(response.status_code, 400)
 
     def test_invalid_email(self):
         """Test an invalid email"""
         response = self.invalid_email()
         result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"],
+        self.assertEqual(result["error"],
                          "martingmail.com is not a valid email")
-        self.assertEqual(response.status_code, 403)
-
-    def test_get_upcoming_meetup(self):
-        """Test fetch upcoming meetups"""
-        self.future_meetups()
-        response = self.get_all_upcoming_meetups()
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["Message"],
-                         "Success")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
 
     def test_empty_strings(self):
         """Test empty string inputs"""
-        self.create_meetup()
         response = self.post_empty_string()
         result = json.loads(response.data.decode())
-        self.assertEqual(result["title"], "Field cannot be empty")
+        self.assertEqual(result["error"], "Please ensure no field is empty")
         self.assertEqual(response.status_code, 400)
 
     def test_no_user_login(self):
@@ -131,5 +51,5 @@ class TestViews(BaseTest):
         self.signup()
         response = self.no_user_login()
         result = json.loads(response.data.decode())
-        self.assertEqual(result["Error"], "User not found: Please register")
+        self.assertEqual(result["error"], "user not found: Please register")
         self.assertEqual(response.status_code, 404)
