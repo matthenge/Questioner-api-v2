e3cc072a4765721b7d9f88c2ecd134164d089026
matthenge <matthenge@gmail.com>
2019-01-21 07:20:21 +0300

    [finishes #163257036]Add a get upcoming meetups endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v2.views.user_views import Users, UserLogin
-from app.api.v2.views.meetup_views import AllMeetups
+from app.api.v2.views.meetup_views import AllMeetups, Upcoming
 
 version2 = Blueprint('v2', __name__, url_prefix='/api/v2')
 
@@ -10,3 +10,4 @@ api = Api(version2, catch_all_404s=True)
 api.add_resource(Users, '/auth/signup', strict_slashes=False)
 api.add_resource(UserLogin, '/auth/login', strict_slashes=False)
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
+api.add_resource(Upcoming, '/meetups/upcoming/', strict_slashes=False)
