746662b888a52fb26d480dda73125b03cad58471
matthenge <matthenge@gmail.com>
2019-01-21 21:56:12 +0300

    [finishes #163256993]Add the get specific meetup endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v2.views.user_views import Users, UserLogin
-from app.api.v2.views.meetup_views import AllMeetups, Upcoming
+from app.api.v2.views.meetup_views import AllMeetups, Upcoming, OneMeetup
 
 version2 = Blueprint('v2', __name__, url_prefix='/api/v2')
 
@@ -11,3 +11,4 @@ api.add_resource(Users, '/auth/signup', strict_slashes=False)
 api.add_resource(UserLogin, '/auth/login', strict_slashes=False)
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
 api.add_resource(Upcoming, '/meetups/upcoming/', strict_slashes=False)
+api.add_resource(OneMeetup, '/meetups/<int:meetupId>', strict_slashes=False)
