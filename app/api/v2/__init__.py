6c2c2a6997fc00bec50a75516223bed79ab5cf55
matthenge <matthenge@gmail.com>
2019-01-22 16:06:12 +0300

    [finishes #163257325]Add question endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v2.views.user_views import Users, UserLogin
 from app.api.v2.views.meetup_views import AllMeetups, Upcoming, OneMeetup
+from app.api.v2.views.question_views import AllQuestions
 
 version2 = Blueprint('v2', __name__, url_prefix='/api/v2')
 
@@ -12,3 +13,4 @@ api.add_resource(UserLogin, '/auth/login', strict_slashes=False)
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
 api.add_resource(Upcoming, '/meetups/upcoming/', strict_slashes=False)
 api.add_resource(OneMeetup, '/meetups/<int:meetupId>', strict_slashes=False)
+api.add_resource(AllQuestions, '/questions', strict_slashes=False)
