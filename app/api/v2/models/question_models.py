0e686e3182bc85c49248bae45ad70242b72ef4bd
matthenge <matthenge@gmail.com>
2019-01-22 23:28:38 +0300

    [feature #163257418]Add the upvote model

--- app/api/v2/models/question_models.py
+++ app/api/v2/models/question_models.py
@@ -35,3 +35,34 @@ class QuestionModels(BaseModels):
         newQuest = json.dumps(newQuest, default=str)
 
         return newQuest
+
+    def upvote(self, questionId, userId):
+        """Method to upvote a question"""
+        table = "questions"
+        tbl = "voters"
+        fields = "questionId, voterId"
+        columns = "votes=votes+1"
+        data = "meetup, title, body, votes"
+        column = "questionId"
+        search_item = questionId
+        values = questionId, userId
+        question = BaseModels.fetch_specific(self, data, table, column,
+                                             search_item)
+        question = json.dumps(question, default=str)
+        question = json.loads(question)
+        if not question:
+            return "no question"
+        user = BaseModels.fetch_some_data(self, fields, tbl, "voterId", userId,
+                                          "questionId", questionId)
+        user = json.dumps(user, default=str)
+        user = json.loads(user)
+        if user:
+            return "voted"
+        updated = BaseModels.update_record(self, table, columns, column,
+                                           search_item)
+        voter = BaseModels.insert_data(self, tbl, fields, questionId, userId)
+        quiz = BaseModels.fetch_specific(self, data, table, column,
+                                         search_item)
+        quiz = json.dumps(quiz, default=str)
+        quiz = json.loads(quiz)
+        return quiz
