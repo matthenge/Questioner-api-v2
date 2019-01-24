f44f8777aee916c43d54e89bf315869794bff52e
matthenge <matthenge@gmail.com>
2019-01-24 06:59:14 +0300

    [feature #163257618]Modify tests

--- app/tests/v2/test_meetups.py
+++ app/tests/v2/test_meetups.py
@@ -7,4 +7,42 @@ import json
 
 class TestMeetups(BaseTest):
     """Test Meetups"""
-    pass
+
+    def test_post_meetup(self):
+        """Test create meetup endpoint"""
+        response = self.meetups()
+        result = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("topic", result.get("meetup"))
+
+    def test_get_meetup(self):
+        """Test get meetup endpoint"""
+        self.meetups()
+        response = self.get_meetups()
+        result = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("location", result.get("meetups"))
+
+    def test_get_upcoming_meetup(self):
+        """Test get upcoming meetup endpoint"""
+        self.meetups()
+        response = self.get_upcoming_meetups()
+        result = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("meetupid", result.get("meetups")[0])
+
+    def test_get_upcoming_meetup(self):
+        """Test delete meetup endpoint"""
+        self.meetups()
+        response = self.delete_meetup()
+        result = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["data"], "Meetup deleted")
+
+    def test_get_all_meetups(self):
+        """Test get all meetups endpoint"""
+        self.meetups()
+        response = self.get_all_meetups()
+        result = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("meetupid", result.get("data")[0])
