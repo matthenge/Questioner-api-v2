e48e45840a8610730eeaa5964c0c851b918265c3
matthenge <matthenge@gmail.com>
2019-01-24 06:58:44 +0300

    [feature #163257618]Modify base tests

--- app/tests/v2/base_tests.py
+++ app/tests/v2/base_tests.py
@@ -81,9 +81,19 @@ class BaseTest(unittest.TestCase):
             "location": "Kisumu",
             "images": "{img1, img2, img3}",
             "topic": "data science",
-            "happeningOn": "2019-01-22 18:30",
+            "happeningOn": "2019-01-26 18:30",
             "tags": "{#bigdata, #ai}"
         }
+        self.comment = {
+            "question": 1,
+            "comment": "data science"
+        }
+        self.question = {
+            "meetup": 1,
+            "title": "data science",
+            "body": "what is data science?"
+        }
+        self.admToken = self.admin_login()
 
     def signup(self):
         """user registration"""
@@ -99,7 +109,8 @@ class BaseTest(unittest.TestCase):
             '/api/v2/auth/login',
             data=json.dumps(self.dbAdmin),
             content_type='application/json')
-        admToken = json.loads(res.data.decode("UTF-8"))
+        print(res.get_json().get("data")[0].get("token"))
+        admToken = res.get_json().get("data")[0].get("token")
         return admToken
 
     def post_empty_string(self):
@@ -114,7 +125,7 @@ class BaseTest(unittest.TestCase):
         """login user"""
         res = self.client.post(
             '/api/v2/auth/login',
-            data=json.dumps(self.login),
+            data=json.dumps(self.dbAdmin),
             content_type='application/json')
         return res
 
@@ -154,8 +165,84 @@ class BaseTest(unittest.TestCase):
         """Create Meetup"""
         res = self.client.post(
            '/api/v2/meetups',
-           headers={"x-access-token": admToken},
+           headers={"x-access-token": "{}".format(self.admToken)},
            data=json.dumps(self.meetup),
+           content_type='application/json'
+        )
+        return res
+
+    def get_meetups(self):
+        """get Meetup"""
+        res = self.client.get(
+           '/api/v2/meetups/1',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def get_upcoming_meetups(self):
+        """get upcoming Meetup"""
+        res = self.client.get(
+           '/api/v2/meetups/upcoming/',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def delete_meetup(self):
+        """Delete Meetup"""
+        res = self.client.delete(
+           '/api/v2/meetups/1',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def get_all_meetups(self):
+        """Get all Meetups"""
+        res = self.client.get(
+           '/api/v2/meetups',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def questions(self):
+        """Post questions"""
+        res = self.client.post(
+           '/api/v2/questions',
+           headers={"x-access-token": "{}".format(self.admToken)},
+           data=json.dumps(self.question),
+           content_type='application/json'
+        )
+        return res
+
+    def get_all_questions(self):
+        """Get all questions"""
+        res = self.client.get(
+           '/api/v2/questions',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def upvote(self):
+        """Upvote questions"""
+        res = self.client.patch(
+           '/api/v2/questions/1/upvote',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def downvote(self):
+        """downvote questions"""
+        res = self.client.patch(
+           '/api/v2/questions/1/downvote',
+           headers={"x-access-token": "{}".format(self.admToken)}
+        )
+        return res
+
+    def comments(self):
+        """Post comments"""
+        res = self.client.post(
+           '/api/v2/comments/',
+           headers={"x-access-token": "{}".format(self.admToken)},
+           data=json.dumps(self.comment),
            content_type='application/json'
         )
         return res
