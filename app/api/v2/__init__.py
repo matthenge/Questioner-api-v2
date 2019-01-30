755c88ae0faae5ac65845ea44f11cd71f643f86d
matthenge <matthenge@gmail.com>
2019-01-30 10:53:04 +0300

    [finishes #163070840]Add logout endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v2.views.user_views import (Users, UserLogin, Promote,
-                                         ResetPassword)
+                                         ResetPassword, UserLogout)
 from app.api.v2.views.meetup_views import AllMeetups, Upcoming, OneMeetup
 from app.api.v2.views.meetup_views import AdminMeetups
 from app.api.v2.views.question_views import AllQuestions, Upvote, Downvote
@@ -15,6 +15,7 @@ api = Api(version2, catch_all_404s=True)
 
 api.add_resource(Users, '/auth/signup', strict_slashes=False)
 api.add_resource(UserLogin, '/auth/login', strict_slashes=False)
+api.add_resource(UserLogout, '/auth/logout', strict_slashes=False)
 api.add_resource(Promote, '/auth/promote/<int:userId>', strict_slashes=False)
 api.add_resource(ResetPassword, '/auth/reset_password', strict_slashes=False)
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
