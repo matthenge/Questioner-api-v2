d4f5ba5489e52d3f7dd079fc8549af73e915caaa
matthenge <matthenge@gmail.com>
2019-02-13 19:05:55 +0300

    [bug #163824977]Add an instance of flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint
 from flask_cors import CORS
+from flask_mail import Mail
 from .api.v2 import version2 as v2
 from app.database import QuestionerDB
 from app import config
+
+mail = Mail()
 
 
 def create_app(configuration="development"):
@@ -13,6 +16,7 @@ def create_app(configuration="development"):
     QuestionerDB.create_tables()
     app.register_blueprint(v2)
     app.url_map.strict_slashes = False
+    mail.init_app(app)
     CORS(app)
 
     return app
